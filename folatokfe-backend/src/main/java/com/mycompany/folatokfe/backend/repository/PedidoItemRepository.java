4b40d1098c8d572e1e8971b8ecfc39924d18b450
minitoonlink-prog <minitoonlink@gmail.com>
2026-06-19 19:15:00 -0500

    Delete folatokfe-backend directory

--- folatokfe-backend/src/main/java/com/mycompany/folatokfe/backend/repository/PedidoItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.folatokfe.backend.repository;
-
-import com.mycompany.folatokfe.backend.model.PedidoItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoItemRepository extends JpaRepository<PedidoItem, Long> {
-}
